18e3132ce3bf3f862ea849171dc325a02bf4d84d
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-03-27 23:50:01 +0100

    Unit tests

--- internal/domain/entities/parking_lot.go
+++ internal/domain/entities/parking_lot.go
@@ -12,7 +12,7 @@ type ParkingLot struct {
 	Location      string // We can make it a more complex type with latitude and longitude, but I don't think it matters for this prototype
 	Capacity      int
 	ParkingSpaces []ParkingSpace
-	// Below are a;; the stats we require for monitoring, they are not persisted
+	// Below are the stats we require for monitoring, they are not persisted.
 	Available int `gorm:"-"`
 	Occupied  int `gorm:"-"`
 	Reserved  int `gorm:"-"`
@@ -26,7 +26,6 @@ func (s *ParkingLot) OnCreate(name, location string, capacity int) {
 	s.Capacity = capacity
 
 	s.ParkingSpaces = make([]ParkingSpace, 0, capacity)
-
 	for i := 0; i < capacity; i++ {
 		space := ParkingSpace{}
 		space.OnCreate(fmt.Sprintf("%s-%d", name, i+1), s.ID)
