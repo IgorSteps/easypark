6ce43a8376b3993180846bedcbd5cd809ede62eb
Filipe677 <158055508+Filipe677@users.noreply.github.com>
2024-03-31 20:10:39 +0100

    Renamed

--- internal/domain/repositories/parking_lot.go
+++ internal/domain/repositories/parking_lot.go
@@ -12,14 +12,8 @@ type ParkingLotRepository interface {
     // CreateParkingLot adds a new parking lot to the database.
     CreateParkingLot(ctx context.Context, parkingLot *entities.ParkingLot) error
 
-    // GetParkingLotByID retrieves a parking lot by its ID.
-    GetParkingLotByID(ctx context.Context, id uuid.UUID) (*entities.ParkingLot, error)
-
     // GetAllParkingLots retrieves all parking lots from the database.
     GetAllParkingLots(ctx context.Context) ([]entities.ParkingLot, error)
-
-    // UpdateParkingLot updates an existing parking lot's details in the database.
-    UpdateParkingLot(ctx context.Context, parkingLot *entities.ParkingLot) error
 
     // DeleteParkingLot removes a parking lot from the database by its ID.
     DeleteParkingLot(ctx context.Context, id uuid.UUID) error
