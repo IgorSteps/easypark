71782126f8187c8aa8d1fea6567cd660db01c436
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-04-12 17:47:11 +0100

    fix unit test

--- internal/adapters/rest/routes/routes_test.go
+++ internal/adapters/rest/routes/routes_test.go
@@ -46,10 +46,11 @@ func TestRoutes_NewRouter_HappyPath(t *testing.T) {
 	mockHandlerFactory.EXPECT().DeleteParkingLot().Return(testHandler).Once()
 	mockHandlerFactory.EXPECT().UpdateParkingSpaceStatus().Return(testHandler).Once()
 	mockHandlerFactory.EXPECT().CreateNotification().Return(testHandler).Once()
+	mockHandlerFactory.EXPECT().GetSingleParkingSpace().Return(testHandler).Twice()
 
 	// This middlware will get executed for very route invocation.
-	mockMiddleware.EXPECT().Authorise(mock.AnythingOfType("http.HandlerFunc")).Return(testHandler).Times(13)
-	mockMiddleware.EXPECT().CheckStatus(mock.AnythingOfType("http.HandlerFunc")).Return(testHandler).Times(4)
+	mockMiddleware.EXPECT().Authorise(mock.AnythingOfType("http.HandlerFunc")).Return(testHandler).Times(15)
+	mockMiddleware.EXPECT().CheckStatus(mock.AnythingOfType("http.HandlerFunc")).Return(testHandler).Times(5)
 	mockMiddleware.EXPECT().RequireRole(entities.RoleDriver).Return(passThroughMiddleware).Once()
 	mockMiddleware.EXPECT().RequireRole(entities.RoleAdmin).Return(passThroughMiddleware).Once()
 
