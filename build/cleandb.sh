03a32c0bbb35e28eca1b8f6226dc103bac7d9fb5
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-04-30 19:10:09 +0000

    figure out db host

--- build/cleandb.sh
+++ build/cleandb.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-
-# DB credentials, same as in docker-compose.
-DB_USER="devUser"
-DB_PASSWORD="devPassword"
-DB_NAME="easypark"
-DB_HOST="database"
-DB_PORT="5432"
-
 TABLES="users,parking_spaces,parking_lots,parking_requests,notifications,alerts"
 
 for TABLE in $TABLES; do
