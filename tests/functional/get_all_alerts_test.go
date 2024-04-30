4a5db9750a555c59ed21dc1c5a78ca0b6dfe0c30
matt hyde <htr21nju@uea.ac.uk>
2024-04-30 14:25:00 +0100

    Draft 9 removed HappyPath_Scheduler

--- tests/functional/get_all_alerts_test.go
+++ tests/functional/get_all_alerts_test.go
@@ -17,7 +17,7 @@ type GetAllAlertsTestSuite struct {
 	client.RestClientSuite
 }
 
-func (s *GetAllAlertsTestSuite) TestGetAllAlerts_HappyPath_HTTPEndpoint() {
+func (s *GetAllAlertsTestSuite) TestGetAllAlerts() {
 	// --------
 	// ASSEMBLE
 	// --------
@@ -102,74 +102,6 @@ func (s *GetAllAlertsTestSuite) TestGetAllAlerts_HappyPath_HTTPEndpoint() {
 	s.Require().Equal(parkingSpaceID, alerts[0].ParkingSpaceID)
 	s.Require().Equal(parkingRequest3.UserID, alerts[1].UserID)
 	s.Require().Equal(parkingSpaceID, alerts[1].ParkingSpaceID)
-}
-
-// Can't test it.
-func (s *GetAllAlertsTestSuite) TestGetAllAlerts_HappyPath_Scheduler() {
-	// --------
-	// ASSEMBLE
-	// --------
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	// Create admin and driver
-	adminToken := utils.CreateAndLoginAdmin(ctx, &s.RestClientSuite)
-	driver, driverToken := utils.CreateAndLoginDriver(ctx, &s.RestClientSuite, nil)
-
-	// Create a parking lot
-	createParkingLot := &models.CreateParkingLotRequest{
-		Name:     "test-lot",
-		Capacity: 5,
-	}
-	parkingLot := utils.CreateParkingLot(ctx, adminToken, createParkingLot, &s.RestClientSuite)
-	parkingSpaceID := parkingLot.ParkingSpaces[0].ID
-
-	// Create parking request that will trigger the alert
-	createParkingRequest := &models.CreateParkingRequestRequest{
-		DestinationParkingLotID: parkingLot.ID,
-		// Times are a little bit in the future, because we don't allow creation of parking request with times in the past.
-		StartTime: time.Now().Add(3 * time.Second), // 5 seconds in the future
-		EndTime:   time.Now().Add(5 * time.Second), // 7 seconds in the future
-	}
-	parkingRequest := utils.CreateParkingRequest(ctx, driverToken, driver.ID, parkingLot.ID, createParkingRequest, &s.RestClientSuite)
-	// Assign that parking request a space we chose above.
-	utils.AssignParkingSpace(ctx, parkingSpaceID, parkingRequest.ID, adminToken, &s.RestClientSuite)
-
-	// Create a request in the distant future that won't trigger the alert.
-	createParkingRequest2 := &models.CreateParkingRequestRequest{
-		DestinationParkingLotID: parkingLot.ID,
-		// Times are a little bit in the future, because we don't allow creation of parking request with times in the past.
-		StartTime: time.Now().Add(5 * time.Hour), // 5 hours in the future
-		EndTime:   time.Now().Add(7 * time.Hour), // 7 hours in the future
-	}
-	parkingRequest2 := utils.CreateParkingRequest(ctx, driverToken, driver.ID, parkingLot.ID, createParkingRequest2, &s.RestClientSuite)
-	// Assign that parking request a space we chose above.
-	utils.AssignParkingSpace(ctx, parkingSpaceID, parkingRequest2.ID, adminToken, &s.RestClientSuite)
-
-	// Create another parking request that will trigger the alert
-	createParkingRequest3 := &models.CreateParkingRequestRequest{
-		DestinationParkingLotID: parkingLot.ID,
-		// Times are a little bit in the future, because we don't allow creation of parking request with times in the past.
-		StartTime: time.Now().Add(6 * time.Second), // 6 seconds in the future
-		EndTime:   time.Now().Add(8 * time.Second), // 8 seconds in the future
-	}
-	parkingRequest3 := utils.CreateParkingRequest(ctx, driverToken, driver.ID, parkingLot.ID, createParkingRequest3, &s.RestClientSuite)
-	// Assign that parking request a space we chose above.
-	utils.AssignParkingSpace(ctx, parkingSpaceID, parkingRequest3.ID, adminToken, &s.RestClientSuite)
-
-	// Sleep for 10 secs.
-	time.Sleep(10 * time.Second)
-
-	// Don't send the arrival notification.
-
-	// --------
-	// ACT
-	// --------
-
-	// --------
-	// ASSERT
-	// --------
-
 }
 
 func TestGetAllAlertsTestSuiteInit(t *testing.T) {
