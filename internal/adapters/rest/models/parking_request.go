c1d32265606ba448d8896a271a956699b7576186
Igor Stepanenko <84919282+IgorSteps@users.noreply.github.com>
2024-05-04 21:06:19 +0100

    Add parking lot name (#40)

--- internal/adapters/rest/models/parking_request.go
+++ internal/adapters/rest/models/parking_request.go
@@ -9,26 +9,29 @@ import (
 
 // CreateParkingRequestRequest represent the data in an incoming HTTP request to create a parking request.
 type CreateParkingRequestRequest struct {
-	DestinationParkingLotID uuid.UUID `json:"destination"`
-	StartTime               time.Time `json:"startTime"`
-	EndTime                 time.Time `json:"endTime"`
+	DestinationParkingLotID   uuid.UUID `json:"destinationLotID"`
+	DestinationParkingLotName string    `json:"destinationLotName"`
+	StartTime                 time.Time `json:"startTime"`
+	EndTime                   time.Time `json:"endTime"`
 }
 
 // ToDomain converts CreateParkingRequestRequest into our domain type.
 func (s *CreateParkingRequestRequest) ToDomain() *entities.ParkingRequest {
 	return &entities.ParkingRequest{
-		DestinationParkingLotID: s.DestinationParkingLotID,
-		StartTime:               s.StartTime,
-		EndTime:                 s.EndTime,
+		DestinationParkingLotID:   s.DestinationParkingLotID,
+		DestinationParkingLotName: s.DestinationParkingLotName,
+		StartTime:                 s.StartTime,
+		EndTime:                   s.EndTime,
 	}
 }
 
 // CreateParkingRequestResponse represent the data in an outgoing HTTP response toa  create parking request request.
 type CreateParkingRequestResponse struct {
-	ID          uuid.UUID                     `json:"id"`
-	UserID      uuid.UUID                     `json:"userId"`
-	Destination uuid.UUID                     `json:"destination"`
-	StartTime   time.Time                     `json:"starttime"`
-	EndTime     time.Time                     `json:"endtime"`
-	Status      entities.ParkingRequestStatus `json:"status"`
+	ID                 uuid.UUID                     `json:"id"`
+	UserID             uuid.UUID                     `json:"userId"`
+	DestinationLotID   uuid.UUID                     `json:"destinationLotId"`
+	DestinationLotName string                        `json:"destinationLotName"`
+	StartTime          time.Time                     `json:"starttime"`
+	EndTime            time.Time                     `json:"endtime"`
+	Status             entities.ParkingRequestStatus `json:"status"`
 }
